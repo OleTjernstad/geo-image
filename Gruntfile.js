a48bc6590adab2ea18eba3a5a11e07774a4520b5
Ole Tjernstad <ott556@gmail.com>
2022-05-04 17:39:22 +0200

    update build and version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,59 @@
-const { CheckerPlugin } = require('awesome-typescript-loader');
-const LiveReloadPlugin = require('webpack-livereload-plugin');
-const path = require('path');
-const swag = require('@ephox/swag');
+const { CheckerPlugin } = require("awesome-typescript-loader");
+const LiveReloadPlugin = require("webpack-livereload-plugin");
+const path = require("path");
+const swag = require("@ephox/swag");
 
 module.exports = (grunt) => {
-  const packageData = grunt.file.readJSON('package.json');
-  const BUILD_VERSION = packageData.version + '-' + (process.env.BUILD_NUMBER ? process.env.BUILD_NUMBER : '0');
-  const libPluginPath = 'lib/main/ts/Main.js';
-  const scratchPluginPath = 'scratch/compiled/plugin.js';
-  const scratchPluginMinPath = 'scratch/compiled/plugin.min.js';
-  const tsDemoSourceFile = path.resolve('src/demo/ts/Demo.ts');
-  const jsDemoDestFile = path.resolve('scratch/compiled/demo.js');
+  const packageData = grunt.file.readJSON("package.json");
+  const BUILD_VERSION =
+    packageData.version +
+    "-" +
+    (process.env.BUILD_NUMBER ? process.env.BUILD_NUMBER : "0");
+  const libPluginPath = "lib/main/ts/Main.js";
+  const scratchPluginPath = "scratch/compiled/plugin.js";
+  const scratchPluginMinPath = "scratch/compiled/plugin.min.js";
+  const tsDemoSourceFile = path.resolve("src/demo/ts/Demo.ts");
+  const jsDemoDestFile = path.resolve("scratch/compiled/demo.js");
 
   grunt.initConfig({
     pkg: packageData,
 
     clean: {
-      dirs: [ 'dist', 'scratch' ]
+      dirs: ["dist", "scratch"],
     },
 
     eslint: {
       options: {
-        fix: grunt.option('fix')
+        fix: grunt.option("fix"),
       },
-      plugin: [ 'src/**/*.ts' ]
+      plugin: ["src/**/*.ts"],
     },
 
     shell: {
-      command: 'tsc'
+      command: "tsc",
     },
 
     rollup: {
       options: {
         treeshake: true,
-        format: 'iife',
+        format: "iife",
         onwarn: swag.onwarn,
         plugins: [
           swag.nodeResolve({
             basedir: __dirname,
-            prefixes: {}
+            prefixes: {},
           }),
-          swag.remapImports()
-        ]
+          swag.remapImports(),
+        ],
       },
       plugin: {
         files: [
           {
             src: libPluginPath,
-            dest: scratchPluginPath
-          }
-        ]
-      }
+            dest: scratchPluginPath,
+          },
+        ],
+      },
     },
 
     uglify: {
@@ -58,10 +61,10 @@ module.exports = (grunt) => {
         files: [
           {
             src: scratchPluginPath,
-            dest: scratchPluginMinPath
-          }
-        ]
-      }
+            dest: scratchPluginMinPath,
+          },
+        ],
+      },
     },
 
     concat: {
@@ -69,96 +72,100 @@ module.exports = (grunt) => {
         options: {
           process: (src) => {
             const buildSuffix = process.env.BUILD_NUMBER
-              ? '-' + process.env.BUILD_NUMBER
-              : '';
+              ? "-" + process.env.BUILD_NUMBER
+              : "";
             return src.replace(
               /@BUILD_NUMBER@/g,
               packageData.version + buildSuffix
             );
-          }
+          },
         },
         // scratchPluginMinPath is used twice on purpose, all outputs will be minified for premium plugins
         files: {
-          'dist/image-plugin/plugin.js': [
-            'src/text/license-header.js',
-            scratchPluginMinPath
+          "dist/geo-image/plugin.js": [
+            "src/text/license-header.js",
+            scratchPluginMinPath,
           ],
-          'dist/image-plugin/plugin.min.js': [
-            'src/text/license-header.js',
-            scratchPluginMinPath
-          ]
-        }
-      }
+          "dist/geo-image/plugin.min.js": [
+            "src/text/license-header.js",
+            scratchPluginMinPath,
+          ],
+        },
+      },
     },
 
     copy: {
       css: {
         files: [
-          { src: [ 'CHANGELOG.txt', 'LICENSE.txt' ], dest: 'dist/image-plugin', expand: true }
-        ]
-      }
+          {
+            src: ["CHANGELOG.txt", "LICENSE.txt"],
+            dest: "dist/geo-image",
+            expand: true,
+          },
+        ],
+      },
     },
 
     webpack: {
       options: {
-        mode: 'development',
-        watch: true
+        mode: "development",
+        watch: true,
       },
       dev: {
         entry: tsDemoSourceFile,
-        devtool: 'source-map',
+        devtool: "source-map",
 
         resolve: {
-          extensions: [ '.ts', '.js' ]
+          extensions: [".ts", ".js"],
         },
 
         module: {
           rules: [
             {
               test: /\.js$/,
-              use: [ 'source-map-loader' ],
-              enforce: 'pre'
+              use: ["source-map-loader"],
+              enforce: "pre",
             },
             {
               test: /\.ts$/,
               use: [
                 {
-                  loader: 'ts-loader',
+                  loader: "ts-loader",
                   options: {
                     transpileOnly: true,
-                    experimentalWatchApi: true
-                  }
-                }
-              ]
-            }
-          ]
+                    experimentalWatchApi: true,
+                  },
+                },
+              ],
+            },
+          ],
         },
 
-        plugins: [ new LiveReloadPlugin(), new CheckerPlugin() ],
+        plugins: [new LiveReloadPlugin(), new CheckerPlugin()],
 
         output: {
           filename: path.basename(jsDemoDestFile),
-          path: path.dirname(jsDemoDestFile)
-        }
-      }
-    }
+          path: path.dirname(jsDemoDestFile),
+        },
+      },
+    },
   });
 
-  require('load-grunt-tasks')(grunt);
-  grunt.loadNpmTasks('@ephox/swag');
+  require("load-grunt-tasks")(grunt);
+  grunt.loadNpmTasks("@ephox/swag");
 
-  grunt.registerTask('version', 'Creates a version file', () => {
-    grunt.file.write('dist/image-plugin/version.txt', BUILD_VERSION);
+  grunt.registerTask("version", "Creates a version file", () => {
+    grunt.file.write("dist/geo-image/version.txt", BUILD_VERSION);
   });
 
-  grunt.registerTask('default', [
-    'clean',
-    'eslint',
-    'shell',
-    'rollup',
-    'uglify',
-    'concat',
-    'copy',
-    'version'
+  grunt.registerTask("default", [
+    "clean",
+    "eslint",
+    "shell",
+    "rollup",
+    "uglify",
+    "concat",
+    "copy",
+    "version",
   ]);
 };
